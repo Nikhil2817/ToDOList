0812c21018d7dfac243cdd8933c9c74bd1eb13ff
Nikhil thakur <nikhil994502776643@gmail.com>
2023-10-27 23:58:40 +0530

    Add files via upload

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,13 @@
+import react from "react";
+function TodoItem(props) {
+  return (
+    <div
+      onClick={() => {
+        props.onChecked(props.id);
+      }}
+    >
+      <li>{props.text}</li>
+    </div>
+  );
+}
+export default TodoItem;
